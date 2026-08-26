149bbe09c6631c3929ff901e9abc1ca13e3392bb
andrewwinters5000 <andrew.ross.winters@liu.se>
2026-08-26 22:13:33 +0000

    Deploying to gh-pages from @ andrewwinters5000/klimakoffer@79ba22592f648b637ad3276d774a69dcdc1ffc4d 🚀

--- assets/scripts/milestone6.jl
+++ assets/scripts/milestone6.jl
@@ -278,12 +278,10 @@ function milestone6()
 
     # Compute temperature in Linköping.
     # Linköping lies approximately in the middle of these four grid points.
-    # annual_temperature_linkoping = (temperature[10, 68, :] +
-    #                                 temperature[10, 69, :] +
-    #                                 temperature[11, 68, :] +
-    #                                 temperature[11, 69, :]) / 4
     annual_temperature_linkoping = (temperature[10, 68, :] +
-                                    temperature[11, 68, :]) / 2
+                                    temperature[10, 69, :])/2# +
+                                    # temperature[11, 68, :] +
+                                    # temperature[11, 69, :]) / 4
     average_temperature_linkoping = sum(annual_temperature_linkoping) / ntimesteps
 
     plot_linkoping = plot_annual_temperature(annual_temperature_linkoping,
